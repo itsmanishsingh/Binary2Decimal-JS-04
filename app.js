315664d3a304d8645727c61103893ab652c4104e
itsmanishsingh <itsmanishsingh6@gmail.com>
2022-12-12 20:43:04 +0530

    Updated Code

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-const userInput = document.getElementById('binary').value;
-console.log(userInput);
-function binarytodecimal(){
-    const decimal = parseInt(userInput,2);
-    console.log(decimal);
-    document.getElementsByClassName('resultvalue').innerText = decimal;
+
+function binarytodecimal() {
+  const userInput = document.getElementById("binary");
+  const userInputVal = +userInput.value;
+  const decimal = parseInt(userInputVal, 2);
+  document.querySelector(".resultvalue").innerText = decimal;
+    
+  // setTimeout( document.querySelector(".resultvalue").innerText = decimal, DELAY );
+  console.log(decimal);
 }
-
-document.getElementsByClassName("btn")[0].addEventListener("click", binarytodecimal());
-
-
-
-
